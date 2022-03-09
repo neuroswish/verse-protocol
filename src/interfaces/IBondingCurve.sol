b18ba1194e34cf97f92ff3ab959999d783c104da
Kiran Cherukuri <kirancherukuri@Kirans-MacBook-Pro.local>
2022-03-09 18:52:21 -0500

    interface pragma update

--- src/interfaces/IBondingCurve.sol
+++ src/interfaces/IBondingCurve.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.10;
+pragma solidity ^0.8.11;
 
 interface IBondingCurve {
     function calculateInitializationReturn(uint256 _price, uint256 _reserveRatio, uint256 _slopeInit)
